6835688d65d2eb19a6a1d40ce9e142ac31facdd4
ksimali <cassim.ali@outlook.fr>
2024-10-31 15:00:51 -0400

    Add comments into Transaction model class

--- bibliothequeapp-jfx/src/model/Transaction.java
+++ bibliothequeapp-jfx/src/model/Transaction.java
@@ -1,31 +1,33 @@
 package model;
 
 public class Transaction {
-	// Attriubtes
+	// Attributs 
 	private String utilisateur;
 	private String livre;
 	private boolean retourne;
 	
 	// Getters and Setters
+	// Recupère le nom de l'utilisateur
 	public String getUtilisateur() {
 		return utilisateur;
 	}
-	
+	// Recupère le titre du livre
 	public String getLivre() {
 		return livre;
 	}
 	
+	// Method qui verifie si le livre a été retourné
 	public boolean isRetourne() {
 		return retourne;
 	}
-	// Constructor
+	// Constructeur avec 2 paramètres
 	public Transaction(String utilisateur, String livre) {
 		this.utilisateur = utilisateur;
 		this.livre = livre;
 		this.retourne = false;
 	}
 	
-	// Method retournerLivre()
+	// Methode retournerLivre() qui marque le livre comme retourné
 	public void retournerLivre() {
 		this.retourne = true;
 	}
