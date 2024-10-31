ba64d5ae604b24370458846f1a707613021f6f84
ksimali <cassim.ali@outlook.fr>
2024-10-31 14:45:11 -0400

    Setup Utilisateur model class

---
+++ bibliothequeapp-jfx/src/model/Utilisateur.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Utilisateur {
+	// Attributes
+	private String nom;
+	
+	// Getters and Setters
+	public String getNom() {
+		return nom;
+	}
+	// Constructor
+	public Utilisateur(String nom) {
+		this.nom = nom;
+	}
+	
+
+}
