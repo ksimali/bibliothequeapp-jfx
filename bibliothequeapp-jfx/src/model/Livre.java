f189d1c912633ca763571e805b2ca570c0acf933
ksimali <cassim.ali@outlook.fr>
2024-10-31 14:42:42 -0400

    Setup Livre model class

---
+++ bibliothequeapp-jfx/src/model/Livre.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Livre {
+	// Attributes
+	private String titre;
+	
+	// Getters and Setters
+	public String getTitre() {
+		return titre;
+	}
+	
+	// Constructor
+	public Livre(String titre) {
+		this.titre = titre;
+	}
+}
