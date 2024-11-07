5acf53e0be3cc4c4569902f05ad2243167bcabc8
ksimali <cassim.ali@outlook.fr>
2024-11-07 02:18:18 -0500

    Update model Transaction.java

--- bibliothequeapp-jfx/src/model/Transaction.java
+++ bibliothequeapp-jfx/src/model/Transaction.java
@@ -1,36 +1,44 @@
 package model;
 
 public class Transaction {
-	// Attributs 
-	private String utilisateur; // Nom du user qui a emprunté le livre
-	private String livre; // titre du livre emprunté
-	private boolean estRetourne; // statut de retour du livre
-	
-	// Getters and Setters
-	// Recupère le nom de l'utilisateur
-	public String getUtilisateur() {
-		return utilisateur;
-	}
-	// Recupère le titre du livre
-	public String getLivre() {
-		return livre;
-	}
-	
-	
-	// Constructeur avec 2 paramètres
-	public Transaction(String utilisateur, String livre) {
-		this.utilisateur = utilisateur;
-		this.livre = livre;
-		this.estRetourne = false;
-	}
-	
-	// Methode qui verifie si le livre a été retourné
-	public boolean isRetourne() {
-		return estRetourne;
-	}
-		
-	// Methode retournerLivre() qui marque le livre comme retourné
-	public void retournerLivre() {
-		this.estRetourne = true;
-	}
+    // Attributs 
+    private Utilisateur utilisateur; // Nom du user qui a emprunté le livre
+    private Livre livre; // titre du livre emprunté
+    private boolean estRetourne; // statut de retour du livre
+    
+    // Getters and Setters
+    // Récupère le nom de l'utilisateur
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    // Récupère le titre du livre
+    public Livre getLivre() {
+        return livre;
+    }
+    
+    // Méthode qui vérifie si le livre a été retourné
+    public boolean isRetourne() {
+        return estRetourne;
+    }
+
+    // Constructeur avec 2 paramètres
+    public Transaction(Utilisateur utilisateur, Livre livre) {
+        this.utilisateur = utilisateur;
+        this.livre = livre;
+        this.estRetourne = false; // Par défaut, l'emprunt n'est pas retourné
+    }
+
+    // Méthode retournerLivre() qui marque le livre comme retourné
+    public void retournerLivre() {
+        this.estRetourne = true;
+    }
+    
+    // Méthode pour afficher l'information de la transaction sous forme de chaîne de caractères
+    @Override
+    public String toString() {
+        return "Transaction [Utilisateur=" + utilisateur.getNom() 
+                + ", Livre=" + livre.getTitre() 
+                + ", Statut=" + (estRetourne ? "Retourne" : "Emprunte") + "]";
+    }
 }
